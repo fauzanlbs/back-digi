01b35aa6444940019042d51ed5e105860ac001cb
Fauzan <fauzanlubis23@gmail.com>
2020-04-04 08:42:41 +0800

    update cors

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -1,19 +1,23 @@
 <?php
 
 namespace App\Http\Middleware;
-use Closure;
-class Cors {
 
-    public function handle($request, Closure $next) {
-        $allowedOrigins = ['http://herokuapp.com', 'http://netlify.com','http://localhost'];
-        $origin = isset($_SERVER['HTTP_ORIGIN']) ? $_SERVER['HTTP_ORIGIN'] : '';
-        if (in_array($origin, $allowedOrigins)) {
-            return $next($request)
-                ->header('Access-Control-Allow-Origin', '*')
-                ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-                ->header('Access-Control-Allow-Headers',' Origin, Content-Type, Accept, Authorization, X-Request-With, cache-control,postman-token, token')
-                ->header('Access-Control-Allow-Credentials',' true');
-        }
-        return $next($request);
+use Closure;
+
+class Cors
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        return $next($request)
+        ->header('Access-Control-Allow-Origin', '*')
+        ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS')
+        ->header('Access-Control-Allow-Headers', 'Content-Type, Authorizations');
     }
 }
